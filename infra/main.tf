93a77b0c630bf7771aa13d4b66788864eacaffbc
Jawgee <s3895662@student.rmit.edu.au>
2024-09-28 23:54:56 +1000

    chore: added terraform infrastructure

---
+++ infra/main.tf
@@ -0,0 +1,71 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+    most_recent = true
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_key_pair" "admin" {
+  key_name = "foo_ec2_key"
+  public_key = file(var.public_key_path)
+}
+
+resource "aws_instance" "foo-server" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  key_name = aws_key_pair.admin.key_name
+  security_groups = [aws_security_group.vm.name]
+
+  tags = {
+    Name = "foo-server"
+  }
+}
+
+resource "aws_security_group" "vm" {
+  name = "vm"
+
+  ingress {
+    from_port = 0
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 0
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
