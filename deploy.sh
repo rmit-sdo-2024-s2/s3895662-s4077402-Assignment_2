d53371167cd2b8b2a1eecac02d88535690ee2166
Kaleb Cole <s4077402@student.rmit.edu.au>
2024-10-08 17:43:54 +1100

    chore: updated the paths for the playbook, command to run it, and added sleep

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,12 @@
 echo "Testing AWS credentials"
 # gets the credentials of the user who is logged in
 aws sts get-caller-identity
-app_playbook_path="ansible/app-playbook.yml"
+
 cd infra
 path_to_ssh_key="foo_ec2_key"
 # generate an SSH key pair
-ssh-keygen -f "${path_to_ssh_key}" -N ""
+rm -f "${path_to_ssh_key}"
+ssh-keygen -t ed25519 -f "${path_to_ssh_key}" -N ""
 
 
 
@@ -23,32 +24,21 @@ terraform validate
 echo "Running terraform apply, get ready to review and approve actions..."
 terraform apply
 
+sleep 30
+
 # connnect to the instance
 foo_server_public_hostname=$(terraform output -raw foo_server_public_hostname)
-echo "App public hostname: ${foo_server_public_hostname}"
+# echo "App public hostname: ${foo_server_public_hostname}"
 # check if the app public hostname is empty
 if [ -z "${foo_server_public_hostname}" ]; then
   echo "Failed to get app public hostname from Terraform output"
   exit 1
 fi
-# if the user doesn't have cloud-init installed, install it
-if ! command -v cloud-init &> /dev/null; then
-  echo "cloud-init not found, installing..."
-  sudo apt-get update
-  sudo apt-get install -y cloud-init
-fi
 
-# TODO: turn this into a while loop with a timeout because cloud-init can take a while
-echo "Waiting for cloud-init to finish... (accept SSH fingerprint if prompted)"
-# wait for cloud-init to finish and we can connect to the instance
-sleep 5
-ssh ubuntu@"${foo_server_public_hostname}" -i "${path_to_ssh_key}" cloud-init status -w
-
-
-# cd to root directory
-cd ../
-
+cd ..
 # run ansible playbook
-path_to_ssh_key="misc/foo_ec2_key"
+path_to_ssh_key="infra/foo_ec2_key"
+foo_playbook_path="ansible/foo-playbook.yml"
+ansible_inventory_path="infra/.ansible-inventory.yml"
 echo "Running Ansible playbook..."
-ansible-playbook -i "${foo_server_public_hostname}," -u ubuntu --private-key="${path_to_ssh_key}" "${app_playbook_path}"
+ansible-playbook "${foo_playbook_path}" -i "${ansible_inventory_path}" --private-key="${path_to_ssh_key}" 
